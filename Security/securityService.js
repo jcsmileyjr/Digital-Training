1304d0d0f36150d57f97e9ee9d5d892924f58557
JC <jcsmileyjr@gmail.com>
2017-08-29 21:07:53 -0500

    Security Service Feature: Security Service
    1. create array of questions and answers objects.
    2. Pass all the unit tests

--- Security/securityService.js
+++ Security/securityService.js
@@ -1,7 +1,7 @@
 /*A service that provides an array of security questions and answers to the security html and controller. */
 myApp.factory('Security', [ function () {
 
-
+    var securityQuestions = [{"question":"first question", "fail":false, "options":[{"answer":"first answer", "correct":true}, {"answer":"second answer", "correct":true}, {"answer":"third answer", "correct":true}]}];
     
     return {
         
--- Security/securityService.spec.js
+++ Security/securityService.spec.js
@@ -10,10 +10,12 @@ describe("Security Service:", function() {
     // setup the IssueCounter service before each unit test
     beforeEach(inject(function(_Security_) {
         Security = _Security_;
+        securityQuestions = Security.getSecurityQuestions();
     }));
     
     describe("getSecurityQuestions()", function(){    
         it('retrieve an array', function(){
+            expect(securityQuestions).toEqual(jasmine.any(Object));
         });    
     });    
     describe("securityQuestons Array", function(){
@@ -24,19 +26,19 @@ describe("Security Service:", function() {
         }); 
         
         it('retrieve an question', function(){
-            
+            expect(securityQuestions[0].question).toEqual('first question');
         });
         
         it('retrieve a array of three answers', function(){
-            
+            expect(securityQuestions[0].options.length).toEqual(3);
         });
         
         it('retrieve a Boolean value from the correct property of an answer object', function(){
-           
+            expect(securityQuestions[0].options[0].correct).toMatch(/true|false/);
         });
         
-        it('retrieve a Boolean value from teh fail property of the questoin object ', function(){
-            
+        it('retrieve a Boolean value from the fail property of the questoin object ', function(){
+            expect(securityQuestions[0].fail).toMatch(/true|false/);            
         });        
     });
 });/*End of Slot Machine service Describe block*/
